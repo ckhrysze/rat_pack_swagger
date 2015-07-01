cf1f449492a272e2f56439896d0155014486b376
Mike Schreiber <michael.schreiber@scientificgames.com>
2015-07-01 15:46:38 -0700

    added support for arrays or file

--- lib/rat_pack_swagger.rb
+++ lib/rat_pack_swagger.rb
@@ -3,34 +3,68 @@ require 'json'
 
 class SwaggerObject 
   instance_methods.each do |m|
-    unless m =~ /^__/ || [:instance_eval, :object_id].include?(m)
+    unless m =~ /^__/ || [:inspect, :instance_eval, :object_id].include?(m)
       undef_method m
     end
   end
 
-  def initialize(**kwargs, &block)
-    @obj ||= {}
-    @obj.merge! kwargs
-    instance_eval &block
-  end
-
-  def method_missing(m, *args, &block)
-    if block_given?
-      @obj[m] = SwaggerObject.new(&block).to_h
+  def initialize(*args, **kwargs, &block)
+    if args.count > 0 && (!kwargs.empty? || block_given?)
+      raise "Cannot give both unnamed arguments AND named arguments or block to Swagger parameter '#{m}'."
+    elsif block_given?
+      @obj = kwargs unless kwargs.empty?
+      instance_eval &block
+    elsif !kwargs.empty?
+      @obj = kwargs
+    elsif args.count > 0
+      @obj = [*args]
     else
-      @obj[m] = args[0]
+      raise "Cannot create SwaggerObject with no arguments."
     end
   end
 
-  def to_h
+  def add(*args, **kwargs)
+    @obj ||= []
+    if !@obj.is_a?(Array)
+      raise "Swagger object must be an array to append data '#{item}'"
+    elsif args.count > 0
+      @obj << [*args, kwargs]
+    else
+      @obj << kwargs
+    end
+  end
+
+  def method_missing(m, *args, **kwargs, &block)
+    @obj ||= {}
+    if block_given?
+      @obj[m] = SwaggerObject.new(**kwargs, &block).get
+    elsif !kwargs.empty?
+      @obj[m] = SwaggerObject.new(**kwargs).get
+    elsif args.count > 1
+      @obj[m] = [*args]
+    elsif args.count == 1
+      @obj[m] = args[0]
+    else
+      raise "Cannot give zero arguments to Swagger key '#{m}'"
+    end
+  end
+
+  def get
     @obj
   end
 end
 
 module Sinatra
   module RatPackSwagger
-    def swagger(**kwargs, &block)
-      @@doc.merge!(kwargs.merge(SwaggerObject.new(&block).to_h))
+    def swagger(*args, **kwargs, &block)
+      @@doc ||= {}
+      if args.count.zero?
+        # assume passing data into method call
+        @@doc.merge!(SwaggerObject.new(**kwargs, &block).get)
+      else
+        # assume single argument is filename of existing json
+        @@doc.merge!(JSON.parse(File.read(args[0])))
+      end
     end
 
     def desc(description)
@@ -39,7 +73,7 @@ module Sinatra
 
     def param(**kwargs, &block)
       @@parameters ||= []
-      @@parameters << kwargs.merge(SwaggerObject.new(&block).to_h)
+      @@parameters << SwaggerObject.new(**kwargs, &block).get
     end
 
     def self.registered(app)
